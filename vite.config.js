61f5e72d93ab7b286e16fd7620c28583fd593ada
BAN Studio <ban-studio@example.local>
2026-06-30 13:42:49 +0800

    Set GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: process.env.GITHUB_REPOSITORY === "kagamirui/ban-studio-site" ? "/ban-studio-site/" : "/",
   plugins: [react()],
   server: {
     allowedHosts: true
